570c5fa42f20ad50e0f585779369af3db5610ffb
Chuloh Park <chulohpark@Chulohui-MacBook-Air.local>
2017-09-05 06:39:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chulohpark/tutorial4/FirstLayout.java
@@ -0,0 +1,22 @@
+package com.example.chulohpark.tutorial4;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chulohpark on 2017. 9. 4..
+ */
+
+public class FirstLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.first_layout, container, false);
+        return v;
+    }
+}
